94736c287f47db28a22db2a9105a4302d0a082b2
Alex Gibson <a.gibson@me.com>
2013-11-24 19:51:40 +0000

    Update notify.js demo

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
 define(function(require) {
-    
+
     'use strict';
 
     var Notify = require('notify');
-
-    function onShowNotification () {
-        console.log('notification is shown!');
-    }
-
-    function onCloseNotification () {
-        console.log('notification is closed!');
-    }
-
-    function onClickNotification () {
-        console.log('notification was clicked!');
-    }
-
-    function onPermissionDenied () {
-        alert('Permission has been denied by the user for this domain');
-    }
+    var Visibility = require('visibility');
 
     document.getElementById('my-button').addEventListener('click', function () {
 
+        function onShowNotification () {
+            console.log('notification is shown!');
+        }
+
+        function onCloseNotification () {
+            console.log('notification is closed!');
+        }
+
+        function onClickNotification () {
+            console.log('notification was clicked!');
+        }
+
+        function onErrorNotification () {
+            console.error('Error showing notification. You may need to request permission.');
+        }
+
+        function onPermissionGranted () {
+            console.log('Permission has been granted by the user');
+            myNotification.show();
+        }
+
+        function onPermissionDenied () {
+            console.warn('Permission has been denied by the user');
+        }
+
         var myNotification = new Notify('Yo dawg!', {
-            body: 'This is an awesome notification', 
-            tag: 'My unique id', 
-            notifyShow: onShowNotification, 
-            notifyClose: onCloseNotification, 
-            notifyClick: onClickNotification, 
+            body: 'This is an awesome notification',
+            tag: 'My unique id',
+            notifyShow: onShowNotification,
+            notifyClose: onCloseNotification,
+            notifyClick: onClickNotification,
+            notifyError: onErrorNotification,
+            permissionGranted: onPermissionGranted,
             permissionDenied: onPermissionDenied
         });
 
-        myNotification.show();
+        if (myNotification.needsPermission()) {
+            myNotification.requestPermission();
+        } else {
+            myNotification.show();
+        }
 
     }, false);
-
 });
--- js/notify.js
+++ js/notify.js
@@ -1,201 +1,52 @@
-(function (root, factory) {
+define(function(require) {
 
     'use strict';
 
-    if (typeof define === 'function' && define.amd) {
-        // AMD environment
-        define('notify', [], function () {
-            return factory(root, document);
+    var Notify = require('notify');
+
+    document.getElementById('my-button').addEventListener('click', function () {
+
+        function onShowNotification () {
+            console.log('notification is shown!');
+        }
+
+        function onCloseNotification () {
+            console.log('notification is closed!');
+        }
+
+        function onClickNotification () {
+            console.log('notification was clicked!');
+        }
+
+        function onErrorNotification () {
+            console.error('Error showing notification. You may need to request permission.');
+        }
+
+        function onPermissionGranted () {
+            console.log('Permission has been granted by the user');
+            myNotification.show();
+        }
+
+        function onPermissionDenied () {
+            console.warn('Permission has been denied by the user');
+        }
+
+        var myNotification = new Notify('Yo dawg!', {
+            body: 'This is an awesome notification',
+            tag: 'My unique id',
+            notifyShow: onShowNotification,
+            notifyClose: onCloseNotification,
+            notifyClick: onClickNotification,
+            notifyError: onErrorNotification,
+            permissionGranted: onPermissionGranted,
+            permissionDenied: onPermissionDenied
         });
-    } else {
-        // Browser environment
-        root.Notify = factory(root, document);
-    }
 
-}(this, function (w, d) {
-
-    'use strict';
-
-    function Notify(title, options) {
-
-        this.title = typeof title === 'string' ? title : null;
-
-        this.options = {
-            icon: '',
-            body: '',
-            tag: '',
-            notifyShow: null,
-            notifyClose: null,
-            notifyClick: null,
-            notifyError: null,
-            permissionDenied: null
-        };
-
-        this.permission = null;
-
-
-        if (!this.isSupported()) {
-            return;
-        }
-
-        if (!this.title) {
-            throw new Error('Notify(): first arg (title) must be a string.');
-        }
-
-        //User defined options for notification content
-        if (typeof options === 'object') {
-
-            for (var i in options) {
-                if (options.hasOwnProperty(i)) {
-                    this.options[i] = options[i];
-                }
-            }
-
-            //callback when notification is displayed
-            if (typeof this.options.notifyShow === 'function') {
-                this.onShowCallback = this.options.notifyShow;
-            }
-
-            //callback when notification is closed
-            if (typeof this.options.notifyClose === 'function') {
-                this.onCloseCallback = this.options.notifyClose;
-            }
-
-            //callback when notification is clicked
-            if (typeof this.options.notifyClick === 'function') {
-                this.onClickCallback = this.options.notifyClick;
-            }
-
-            //callback when notification throws error
-            if (typeof this.options.notifyError === 'function') {
-                this.onErrorCallback = this.options.notifyError;
-            }
-
-            //callback user denies permission for notification
-            if (typeof this.options.permissionDenied === 'function') {
-                this.onPermissionDeniedCallback = this.options.permissionDenied;
-            }
-        }
-    }
-
-    Notify.prototype.requestPermission = function () {
-        var that = this;
-        w.Notification.requestPermission(function (perm) {
-            that.permission = perm;
-            switch (that.permission) {
-            case 'granted':
-                that.showNotification();
-                break;
-            case 'denied':
-                that.onPermissionDenied();
-                break;
-            }
-        });
-    };
-
-    Notify.prototype.show = function () {
-        if (!this.isSupported()) { return; }
-
-        if (w.Notification) {
-            if (this.permission === 'granted') {
-                this.showNotification();
-            } else {
-                this.requestPermission();
-            }
+        if (myNotification.needsPermission()) {
+            myNotification.requestPermission();
         } else {
-            this.showNotification();
-        }
-    };
-
-    Notify.prototype.showNotification = function () {
-
-        if (w.Notification) {
-            this.myNotify = new Notification(this.title, {
-                'body': this.options.body,
-                'tag' : this.options.tag,
-                'icon' : this.options.icon
-            });
-
-            this.myNotify.addEventListener('show', this, false);
-            this.myNotify.addEventListener('error', this, false);
-
-        } else {
-            this.myNotify = navigator.mozNotification.createNotification(
-                this.title,
-                this.options.body
-            );
-            this.myNotify.show();
+            myNotification.show();
         }
 
-        this.myNotify.addEventListener('close', this, false);
-        this.myNotify.addEventListener('click', this, false);
-    };
-
-    Notify.prototype.onShowNotification = function () {
-        if (this.onShowCallback) {
-            this.onShowCallback();
-        }
-    };
-
-    Notify.prototype.onCloseNotification = function () {
-        if (this.onCloseCallback) {
-            this.onCloseCallback();
-        }
-        this.destroy();
-    };
-
-    Notify.prototype.onClickNotification = function () {
-        if (this.onClickCallback) {
-            this.onClickCallback();
-        }
-    };
-
-    Notify.prototype.onErrorNotification = function () {
-        if (this.onErrorCallback) {
-            this.onErrorCallback();
-        }
-        this.destroy();
-    };
-
-    Notify.prototype.onPermissionDenied = function () {
-        if (this.onPermissionDeniedCallback) {
-            this.onPermissionDeniedCallback();
-        }
-    };
-
-    Notify.prototype.destroy = function () {
-        if (w.Notification) {
-            this.myNotify.removeEventListener('show', this, false);
-            this.myNotify.removeEventListener('error', this, false);            
-        }
-        this.myNotify.removeEventListener('close', this, false);
-        this.myNotify.removeEventListener('click', this, false);
-    };
-
-    Notify.prototype.isSupported = function () {
-        if (w.Notification || 'mozNotification' in navigator) {
-            return true;
-        }
-        return false;
-    };
-
-    Notify.prototype.handleEvent = function (e) {
-        switch (e.type) {
-        case 'show':
-            this.onShowNotification(e);
-            break;
-        case 'close':
-            this.onCloseNotification(e);
-            break;
-        case 'click':
-            this.onClickNotification(e);
-            break;
-        case 'error':
-            this.onErrorNotification(e);
-            break;
-        }
-    };
-
-    return Notify;
-
-}));
+    }, false);
+});
