5a7eaf77229ad9a68f7388226a12afaf45408db6
Alex Gibson <agibson@mozilla.com>
2015-09-10 11:14:24 +0100

    Update Karma to v0.13.9

--- karma.conf.js
+++ karma.conf.js
@@ -1,69 +1,58 @@
-// Karma configuration
-// Generated on Sun Aug 04 2013 21:16:15 GMT+0100 (BST)
+module.exports = function(config) {
+    config.set({
+        // Karma configuration
 
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+        frameworks: ['jasmine'],
 
+        // list of files / patterns to load in the browser
+        files: [
+            'notify.js',
+            'test/*.js',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'notify.js',
-  'test/*.js',
+            { pattern: 'node_modules/sinon/pkg/sinon.js', watched: false, included: true }
+        ],
 
-  { pattern: 'node_modules/sinon/pkg/sinon.js', watched: false, included: true }
-];
+        // list of files to exclude
+        exclude: [],
 
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'coverage'
+        reporters: ['dots'],
 
-// list of files to exclude
-exclude = [
+        // web server port
+        port: 9876,
 
-];
+        // cli runner port
+        runnerPort: 9100,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        //logLevel: console.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-// web server port
-port = 9876;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Firefox'],
 
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
+};
--- test/tests.js
+++ test/tests.js
@@ -1,47 +1,53 @@
-describe('instantiation', function () {
+/* global Notify, describe, beforeEach, afterEach, it, expect, sinon, spyOn, jasmine */
 
-    it('should create a new Notify instance', function () {
+describe('instantiation', function() {
+
+    'use strict';
+
+    it('should create a new Notify instance', function() {
         var notification = new Notify('foo');
         expect(notification instanceof window.Notify).toBeTruthy();
     });
 
-    it('should throw an exception if has no title', function () {
-        expect(function () {
+    it('should throw an exception if has no title', function() {
+        expect(function() {
             var notification = new Notify();
         }).toThrow();
     });
 });
 
-describe('permission', function () {
+describe('permission', function() {
 
-    it('should check if permission is needed', function () {
+    'use strict';
+
+    it('should check if permission is needed', function() {
         expect(typeof Notify.needsPermission).toBe('boolean');
     });
 
-    it('should request permission from the user', function () {
+    it('should request permission from the user', function() {
         spyOn(window.Notification, 'requestPermission');
         Notify.requestPermission();
         expect(window.Notification.requestPermission).toHaveBeenCalled();
     });
 
-    it('should update Notify.needsPermission to false if the user accepts the request', function () {
-        spyOn(window.Notification, 'requestPermission').andCallFake(function (cb) {
-            cb('granted');
+    describe('needsPermission', function() {
+        beforeEach(function(done) {
+            spyOn(window.Notification, 'requestPermission').and.callFake(function(cb) {
+                cb('granted');
+            });
+            Notify.needsPermission = true;
+            Notify.requestPermission(done);
         });
 
-        runs(Notify.requestPermission);
-
-        waitsFor(function () {
-            return Notify.needsPermission === false;
-        }, 'Notify.needsPermission to be false', 750);
-
-        runs(function () {
+        it('should update Notify.needsPermission to false if the user accepts the request', function() {
             expect(Notify.needsPermission).toBe(false);
         });
     });
 });
 
-describe('callbacks', function () {
+describe('callbacks', function() {
+
+    'use strict';
 
     var callback;
 
@@ -49,7 +55,7 @@ describe('callbacks', function () {
         callback = jasmine.createSpy();
     });
 
-    it('should fire show callback', function () {
+    it('should fire show callback', function() {
         var notification = new Notify('foo', {
             notifyShow: callback
         });
@@ -57,7 +63,7 @@ describe('callbacks', function () {
         expect(callback).toHaveBeenCalled();
     });
 
-    it('should fire close callback', function () {
+    it('should fire close callback', function() {
         var notification = new Notify('foo', {
             notifyClose: callback
         });
@@ -66,7 +72,7 @@ describe('callbacks', function () {
         expect(callback).toHaveBeenCalled();
     });
 
-    it('should fire click callback', function () {
+    it('should fire click callback', function() {
         var notification = new Notify('foo', {
             notifyClick: callback
         });
@@ -74,7 +80,7 @@ describe('callbacks', function () {
         expect(callback).toHaveBeenCalled();
     });
 
-    it('should fire error callback', function () {
+    it('should fire error callback', function() {
         var notification = new Notify('foo', {
             notifyError: callback
         });
@@ -83,7 +89,7 @@ describe('callbacks', function () {
         expect(callback).toHaveBeenCalled();
     });
 
-    it('should destroy a notification once closed', function () {
+    it('should destroy a notification once closed', function() {
         var notification = new Notify('foo', {
             notifyClose: callback
         });
@@ -94,20 +100,28 @@ describe('callbacks', function () {
     });
 });
 
-describe('timeout', function () {
+describe('timeout', function() {
 
-    beforeEach(function () {
-        jasmine.Clock.useMock();
+    'use strict';
+
+    var clock;
+
+    beforeEach(function() {
+        clock = sinon.useFakeTimers();
     });
 
-    it('should close a notification automatically', function () {
+    afterEach(function() {
+        clock.restore();
+    });
+
+    it('should close a notification automatically', function() {
         var notification = new Notify('foo', {
             timeout: 1
         });
         spyOn(window.Notification.prototype, 'close');
         notification.show();
         expect(window.Notification.prototype.close).not.toHaveBeenCalled();
-        jasmine.Clock.tick(1000);
+        clock.tick(1000);
         expect(window.Notification.prototype.close).toHaveBeenCalled();
     });
 });
